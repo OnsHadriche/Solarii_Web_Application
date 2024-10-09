d15b09119ffc7d3428c5db55d225e2a87e609eb2
ons <ounshadriche@gmail.com>
2024-10-10 00:25:52 +0300

    Add images and create a constant file for exporting images

---
+++ src/constants/images.js
@@ -0,0 +1,8 @@
+import formation from "../assets/images/formation.jpg"
+import panneauSolaire from "../assets/images/panneau solaire.jpg"
+import panneauV from "../assets/images/pv.jpg"
+export default {
+    formation,
+    panneauSolaire,
+    panneauV
+}
